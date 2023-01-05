6c91661fa0ee23052200a55ba7bd71f5d29f01a8
Balazs Nagy <julsevern@gmail.com>
2023-01-05 20:25:40 +0100

    git shell prompt colors have zero width

--- shrc.d/prompt.bash
+++ shrc.d/prompt.bash
@@ -1,3 +1,3 @@
 export PS1='\\u@\h [\[\e[34m\]\w\[\e[00m\]] \$ '
 
-__git_install_prompt '\\u@\h [\[\e[34m\]\w\[\e[00m\]]%s \$ ' ' \e[33m‹%s›\e[00m'
+__git_install_prompt '\\u@\h [\[\e[34m\]\w\[\e[00m\]]%s \$ ' ' \[\e[33m\]‹%s›\[\e[00m\]'
